f1bed11ad70c05e8a695d5c1894239805bfb9e50
Rory Savage <rory.christina.savage@gmail.com>
2014-10-29 19:06:13 -0400

    Update elb.rb

--- recipes/elb.rb
+++ recipes/elb.rb
@@ -10,34 +10,34 @@
 # Install required packages for use with aws_sdk
 # This is the RPM Check Section
 execute "digitaldreams-rpm-check: nokogiri-1.6.2.1-1" do
-  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreamsjira.com/common/rubygem-nokogiri-1.6.2.1-1.x86_64.rpm"
+  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreams.com/common/rubygem-nokogiri-1.6.2.1-1.x86_64.rpm"
   not_if "rpm -qa | grep -q 'rubygem-nokogiri-1.6.2.1-1.x86_64'"
   action :nothing
 end.run_action(:run)
 execute "digitaldreams-rpm-check: mini_portile-0.6.0-1" do
-  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreamsjira.com/common/rubygem-mini_portile-0.6.0-1.noarch.rpm"
+  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreams.com/common/rubygem-mini_portile-0.6.0-1.noarch.rpm"
   not_if "rpm -qa | grep -q 'rubygem-mini_portile-0.6.0-1.noarch'"
   action :nothing
 end.run_action(:run)
 execute "digitaldreams-rpm-check: aws-sdk-1.42.0-1" do
-  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreamsjira.com/common/rubygem-aws-sdk-1.42.0-1.noarch.rpm"
+  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreams.com/common/rubygem-aws-sdk-1.42.0-1.noarch.rpm"
   not_if "rpm -qa | grep -q 'rubygem-aws-sdk-1.42.0-1.noarch'"
   action :nothing
 end.run_action(:run)
 
 # This is the GEM Check Section
 execute "digitaldreams-gem-check: nokogiri-1.6.2.1-1" do
-  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreamsjira.com/common/rubygem-nokogiri-1.6.2.1-1.x86_64.rpm"
+  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreams.com/common/rubygem-nokogiri-1.6.2.1-1.x86_64.rpm"
   not_if "/opt/chef/embedded/bin/gem list --local nokogiri|grep -q nokogiri"
   action :nothing
 end.run_action(:run)
 execute "digitaldreams-gem-check: mini_portile-0.6.0-1" do
-  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreamsjira.com/common/rubygem-mini_portile-0.6.0-1.noarch.rpm"
+  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreams.com/common/rubygem-mini_portile-0.6.0-1.noarch.rpm"
   not_if "/opt/chef/embedded/bin/gem list --local mini_portile|grep -q mini_portile"
   action :nothing
 end.run_action(:run)
 execute "digitaldreams-gem-check: aws-sdk-1.42.0-1" do
-  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreamsjira.com/common/rubygem-aws-sdk-1.42.0-1.noarch.rpm"
+  command "rpm -ivh --nodeps --force http://yumrepo.digitaldreams.com/common/rubygem-aws-sdk-1.42.0-1.noarch.rpm"
   not_if "/opt/chef/embedded/bin/gem list --local aws-sdk|grep -q aws-sdk"
   action :nothing
 end.run_action(:run)
